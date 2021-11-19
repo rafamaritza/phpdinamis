0bc4bb5686b9471a5aa175c7278ba2f989259826
rafamaritza <rafamaritza456@gmail.com>
2021-11-19 07:46:20 +0700

    versi 2

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  </head>
+    <body>
+      <div class="container">
+
+        <!-- ini adalah pembuka navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">caca</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Alamat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Hobi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cita cita</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container">
+  <div class="row">
+    <div class="col-4">
+    <div class="card mx-auto my-auto" style="width: 10rem;">
+  <img src="assets/img/caca.pg.jpg" class="card-img-top" style="with: 9rem;">
+  <div class="card-body">
+    <p class="card-text">Caca</p>
+  </div>
+</div>
+    </div>
+    <div class="col-8">
+      <h1>Selamat datang di website saya</h1>
+    <p>Perkenalkan saya Rafa Maritza ,panggilan saya caca , saya kelas XI RPL</p>  
+    </div>
+    
+  </div>
+</div>
+<!-- ini penutup navbar -->
+</div>
+
+    
+    <!-- LINK Javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+ 
+  </body>
+</html>
